1d7e78e887fef923c2117be1a76efafaa03d513e
Nattha <gamenattha555@gmail.com>
2026-01-11 00:40:13 +0700

    add screen and some plugin

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,25 @@
 import './style.css'
+// import { setupCounter } from './components/counter.jsx'
+import GameScreen from './view/gameScreen.jsx'
+import { createRoot } from 'react-dom/client';
+import * as React from "react"
 
-import { setupCounter } from './counter.jsx'
+// document.querySelector('#app').innerHTML = 
+ 
+// `
+//   <div>
 
-document.querySelector('#app').innerHTML = `
-  <div>
-   
-   
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite logo to learn more
+//     </p>
+//   </div>
+// `
 
-setupCounter(document.querySelector('#counter'))
+// setupCounter(document.querySelector('#counter'))
+
+createRoot(document.getElementById("root")).render(
+  <GameScreen />
+);
---
+++ src/view/gameScreen.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function GameScreen() {
+    return (
+        <div className="w-full h-full text-center text-8xl">
+            <h1>Animal clicker</h1>
+        </div>
+    );
+
+}
